719bb641c3a14497ff115f78ed11d645680e20f5
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-09-09 16:36:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 from dotenv import load_dotenv
 import os
-
-load_dotenv()
-
-api_secret = os.getenv("Llama-2-13b-hf-secret")
-
+import json
 import requests
 
-API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-2-13b-hf"
-headers = {"Authorization": "Bearer " + api_secret}
+load_dotenv()
+api_secret = os.getenv("Llama-2-13b-hf-secret")
 
+API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
+headers = {"Authorization": f"Bearer {api_secret}"}
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
 	
 output = query({
-	"inputs": "Can you please let us know more details about your ",
+	"inputs": {
+		"past_user_inputs": ["Which movie is the best ?"],
+		"generated_responses": ["It's Die Hard for sure."],
+		"text": "Can you explain why ?"
+	},
 })
 print(output)
